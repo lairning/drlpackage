2c0cc51e1d858fd94237a81b3e08e89898590acd
lairning <lairning@gmail.com>
2021-08-02 17:24:49 +0100

    Correct an import

--- src/lairningdecisions/trainer/__init__.py
+++ src/lairningdecisions/trainer/__init__.py
@@ -1,4 +1,4 @@
 from gym.spaces import Box, Discrete
-from lairningdecisions.server.api import AISimpyAgent, AIDataAgent, SimpyModel
+from lairningdecisions.trainer.api import AISimpyAgent, AIDataAgent, SimpyModel, SimpyEnv, DataEnv
 
-__all__ = ["AISimpyAgent", "AIDataAgent", "SimpyModel", "Box", "Discrete"]
+__all__ = ["AISimpyAgent", "AIDataAgent", "SimpyModel", "Box", "Discrete", "SimpyEnv", "DataEnv"]
