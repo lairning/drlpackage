a1bc926a753dbdaaf03f046ef78925f0439e640c
lairning <lairning@gmail.com>
2021-08-06 16:45:22 +0100

    # KPIs were added

--- templates/simpy_template/models/job_shop.py
+++ templates/simpy_template/models/job_shop.py
@@ -1,15 +1,12 @@
 import itertools
-import random
-
 import numpy as np
 import simpy
 from lairningdecisions.trainer import SimpyModel, Box
-from scipy.stats import truncnorm
 
 MACHINES = ['M1', 'M2', 'M3']
 PRODUCTS = ['P1', 'P2', 'P3']
 JOB_DURATION = {'M1': {'P1': 5, 'P2': 3, 'P3': 2},
-                'M2': {'P1': 3, 'P2': 3, 'P3': 2},
+                'M2': {'P1': 3, 'P2': 5, 'P3': 2},
                 'M3': {'P1': 3, 'P2': 4, 'P3': 6}}
 
 # Mandatory
@@ -18,26 +15,9 @@ BASE_CONFIG = {
     "ACTION_INTERVAL": 1,  # Time in minutes between each action
     "JOB_DURATION": JOB_DURATION,
     "PRODUCT_REVENUE": {'P1': 10, 'P2': 8, 'P3': 5},
-    "DEMAND_PROBABILITY": {'P1': 1 / 48, 'P2': 1 / 24, 'P3': 1 / 16},
-    "STOCK_COST": 0.02,
-    "END_COST": 5,
+    "DEMAND_PROBABILITY": {'P1': 1 / 10, 'P2': 1 / 10, 'P3': 1 / 10},
+    "STOCK_COST": 0.02
 }
-
-PENALTY_INVALID_ACTION = - 30
-
-DEBUG = False
-
-
-def dprint(*args):
-    if DEBUG:
-        print(*args)
-
-
-def hot_encode(n, N):
-    encode = [0] * N
-    encode[n] = 1
-    return encode
-
 
 N_ACTIONS = 1 + len(PRODUCTS)  # 0 - Nothing; 1 - Product 1; 2 - Product 2; 3 - Product 3
 # Observation:
@@ -63,19 +43,30 @@ class SimModel(SimpyModel):
     def __init__(self, config: dict = None):
         super().__init__(BASE_CONFIG, config)
         self.stock = {p: simpy.Container(self, init=0) for p in PRODUCTS}  # Infinite capacity
-        self.machine = {m: simpy.Resource(self) for m in MACHINES}
+        self.machine = {m: simpy.Resource(self, capacity=1) for m in MACHINES}
         for p in PRODUCTS:
             self.process(self.demand_generator(p))
-        self.revenue_step = 0
+        self.sales_revenue = 0
         self.machine_start_time = {m: -1 for m in MACHINES}  # -1 means it's stopped
         self.machine_product = {m: "" for m in MACHINES}  # "" means no product
+        self.kpis = {'Qtd': {'P1': 0, 'P2': 0, 'P3': 0},
+                     'Duration': {'P1': 0, 'P2': 0, 'P3': 0},
+                     'Revenue': 0, 'Cost': 0
+                     }
 
+    # Agent gets the state of the environment
     def get_observation(self):
         def time_to_finish(machine):
-            if self.machine_start_time[machine] == -1:
+            # if self.machine_start_time[machine] == -1:
+            if self.machine_product[machine] == "":
                 return 0
             product = self.machine_product[machine]
-            return self.sim_config['JOB_DURATION'][machine][product] - (self.now - self.machine_start_time[machine])
+            ttf = self.sim_config['JOB_DURATION'][machine][product] - (self.now - self.machine_start_time[machine])
+            if ttf < 0:
+                raise Exception("Invalid Time to Finish: machine={}, product={}, now={}, start={}, duration={}".format(
+                    machine, product, self.now, self.machine_start_time[machine],
+                    self.sim_config['JOB_DURATION'][machine][product]))
+            return ttf
 
         def product_encode(machine):
             p_encode = [0] * len(PRODUCTS)
@@ -90,34 +81,41 @@ class SimModel(SimpyModel):
         env_status += [self.sim_config['SIM_DURATION'] - self.now]
         return env_status
 
-    def get_reward(self):
-        revenue = self.revenue_step
-        self.revenue_step = 0
-        return revenue, self.done(), {}  # Reward, Done, Info
-
-    # Executes an action
+    # Agent Executes an action
     def exec_action(self, action):
         if action and self.machine['M1'].count:
             # The machine is not free
-            self.revenue_step = PENALTY_INVALID_ACTION
             return
         if action:
             self.process(self.job_schedule(PRODUCTS[action - 1]))
 
-    # Process: Schedule the production of a Product
+    # Agent Get the reward
+    def get_reward(self):
+        stock_cost = sum([self.stock[p].level * self.sim_config["PRODUCT_REVENUE"][p] * self.sim_config["STOCK_COST"]
+                          for p in PRODUCTS])
+        revenue = self.sales_revenue - stock_cost
+        self.kpis['Revenue'] += self.sales_revenue
+        self.kpis['Cost'] += stock_cost
+        self.sales_revenue = 0
+        return revenue, self.done(), self.kpis  # Reward, Done, Info
+
+    # Product Schedule definition
     def job_schedule(self, product):
         """Schedule the production for each product sequentaly starting in M1."""
         for machine in MACHINES:
             with self.machine[machine].request() as req:
+                yield req
                 self.machine_start_time[machine] = self.now
                 self.machine_product[machine] = product
-                yield req
+                if machine == 'M1':
+                    start = self.now
                 yield self.timeout(self.sim_config['JOB_DURATION'][machine][product])
                 self.machine_start_time[machine] = -1
                 self.machine_product[machine] = ""
+        self.kpis['Duration'][product] += self.now - start
         yield self.stock[product].put(1)
 
-    # Generator: Generate product requests
+    # Demand generation model
     def demand_generator(self, product):
         """Generate new product requests that arrive at the factory."""
         for _ in itertools.count():
@@ -125,12 +123,14 @@ class SimModel(SimpyModel):
             amount = np.random.binomial(1, self.sim_config["DEMAND_PROBABILITY"][product])  # Bernouli distribution
             if amount and self.stock[product].level:
                 yield self.stock[product].get(1)
-                self.revenue_step += self.sim_config['PRODUCT_REVENUE'][product]
+                self.sales_revenue += self.sim_config['PRODUCT_REVENUE'][product]
+                self.kpis['Qtd'][product] += 1
 
 
-# Mandatory
+# To benchmark the AI Agent Performance
 class SimBaseline:
-    def __init__(self, baseline_config: dict = None, sim_config: dict = None):
+    def __init__(self, baseline_config: dict = None,
+                 sim_config: dict = None):
         self.sim = None
         self.baseline_config = dict()
         if baseline_config is not None:
@@ -138,13 +138,14 @@ class SimBaseline:
         self.sim_config = BASE_CONFIG.copy()
         if sim_config is not None:
             self.sim_config.update(sim_config)
+        self.kpis = dict()
 
     def get_action(self, obs):
         if self.sim.machine_product['M1'] != "":
             return 0
         product_stock = obs[:len(PRODUCTS)]
         min_stock = min(product_stock)
-        if min_stock >= 3:
+        if min_stock >= 1:
             return 0
         for i, s in enumerate(product_stock):
             if s == min_stock:
@@ -159,23 +160,32 @@ class SimBaseline:
             obs = self.sim.get_observation()
             action = self.get_action(obs)
             self.sim.exec_action(action)
-            reward, done, _ = self.sim.get_reward()
+            reward, done, kpis = self.sim.get_reward()
             total_reward += reward
 
+        self.kpis = kpis
         return total_reward
 
 
-def print_stats(sim: SimModel):
-    pass
-
-
+# Used for testing the simulator and the baseline
 if __name__ == "__main__":
-    n = 40
-    total = 0
+    n = 50
+    revenue = 0
+    cost = 0
+    qtd = {'P1': 0, 'P2': 0, 'P3': 0}
+    duration = {'P1': 0, 'P2': 0, 'P3': 0}
     for i in range(n):
         baseline = SimBaseline()
-        reward = baseline.run()
-        total += reward
-        # print_stats(baseline.sim)
-        # print("# {}: {}".format(i, reward))
-    print("### Average Rewards", total / n)
+        baseline.run()
+        revenue += baseline.kpis['Revenue']
+        cost += baseline.kpis['Cost']
+        avg_duration = dict()
+        for p in PRODUCTS:
+            qtd[p] += baseline.kpis['Qtd'][p]
+            duration[p] += baseline.kpis['Duration'][p]
+            avg_duration[p] = baseline.kpis['Duration'][p] / baseline.kpis['Qtd'][p]
+        # print("# Revenue={}; Cost={}; Avg Duration={}".format(baseline.kpis['Revenue'],
+        #                                                       baseline.kpis['Cost'], avg_duration))
+    for p in PRODUCTS:
+        avg_duration[p] = duration[p] / qtd[p]
+    print("# TOTAL # Avg Revenue={}; Avg Cost={}; Avg Duration={}".format(revenue / n, cost / n, avg_duration))
